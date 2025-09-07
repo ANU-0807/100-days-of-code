d4540aab01c933cce6c8458111c324811020f574
ANU-0807 <anoushkasaini2007@gmail.com>
2025-09-07 15:19:33 +0530

    Update A.c

--- DAY 2/A.c
+++ DAY 2/A.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+int main()
+{
+int l, b, a, p;
 
+printf("Enter length and breadth of rectangle:", l, b);
+scanf("%d %d", &l, &b);
+a = l*b;
+p = 2*(l+b);
+printf("The area of rectangle is: %d\n", a);
+printf("The perimeter of rectangle is: %d\n", p);
+return 0;
+}
