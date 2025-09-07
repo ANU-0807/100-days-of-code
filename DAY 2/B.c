e51085b15b26a31af78873e1c271504ab740f1ab
ANU-0807 <anoushkasaini2007@gmail.com>
2025-09-07 15:19:54 +0530

    Update B.c

--- DAY 2/B.c
+++ DAY 2/B.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+int main()
+{
+int r, a, c ;
 
+printf("Enter radius of circle:", r);
+scanf("%d", &r);
+a = 3.14 * r * r;
+c = 2 * 3.14 * r;
+printf("The area of cirlce is: %d\n", a);
+printf("The circumference of circle is: %d\n", c);
+return 0;
+}
