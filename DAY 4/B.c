a410d25d7f0bf5816b668c19aafe78fc56251634
ANU-0807 <anoushkasaini2007@gmail.com>
2025-09-16 11:56:55 +0530

    Create B.c

---
+++ DAY 4/B.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+int n, i, sum = 0;
+
+printf("Enter the value of n: ");
+scanf("%d", &n);
+
+for(i = 1; i<=n; i++) {
+sum += i;
+}
+printf("The sum of first %d natural numbers is: %d\n", n, sum);
+
+return 0;
+}
