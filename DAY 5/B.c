63957dcfa79c20314bf8efc24201708394a1a76b
ANU-0807 <anoushkasaini2007@gmail.com>
2025-10-03 18:22:32 +0530

    Update B.c

--- DAY 5/B.c
+++ DAY 5/B.c
@@ -1,1 +1,16 @@
+//Q10. Write a program to input time in seconds and convert it to hours:minutes: seconds format.
+#include <stdio.h>
+int main()
+{
+int time, hr, min, sec;
+printf("Enter the time in seconds:");
+scanf("%d", &time);
+
+hr = time/3600;
+min = (time% 3600)/60;
+sec= time%60;
+
+printf("The time in hr:min:sec is %d:%d:%d\n", hr, min, sec);
+return 0;
+}
 
