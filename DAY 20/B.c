1d191491e3c22c5e5ff33968c9fbef2ed2defa34
ANU-0807 <anoushkasaini2007@gmail.com>
2025-11-03 10:23:54 +0530

    Update B.c

--- DAY 20/B.c
+++ DAY 20/B.c
@@ -1,1 +1,18 @@
+// Q40. Write a program to find the 1’s complement of a binary number and print it.
 
+#include <stdio.h>
+int main() {
+    long long bin;
+    printf("Enter binary number: ");
+    scanf("%lld", &bin);
+    long long comp = 0, place = 1, digit;
+    while (bin != 0) {
+        digit = bin % 10;
+        if (digit == 0)
+            comp += 1 * place;
+        place *= 10;
+        bin /= 10;
+    }
+    printf("1's complement = %lld", comp);
+    return 0;
+}
