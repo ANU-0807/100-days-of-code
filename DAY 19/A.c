c32d3d405642711deefbe50b7b12311ceabc2ff2
ANU-0807 <anoushkasaini2007@gmail.com>
2025-11-03 10:21:38 +0530

    Update A.c

--- DAY 19/A.c
+++ DAY 19/A.c
@@ -1,1 +1,19 @@
+// Q37. Write a program to find the LCM of two numbers.
 
+#include <stdio.h>
+int main() {
+    int a, b, hcf, lcm, x, y;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    x = a; y = b;
+    while (a != b) {
+        if (a > b)
+            a -= b;
+        else
+            b -= a;
+    }
+    hcf = a;
+    lcm = (x * y) / hcf;
+    printf("LCM = %d", lcm);
+    return 0;
+}
