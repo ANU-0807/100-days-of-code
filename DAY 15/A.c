19ab555cfed35a03ea9a0d2e27bf621baa5fe04c
ANU-0807 <anoushkasaini2007@gmail.com>
2025-11-03 10:14:21 +0530

    Update A.c

--- DAY 15/A.c
+++ DAY 15/A.c
@@ -1,1 +1,15 @@
+// Q29. Write a program to calculate the factorial of a number.
 
+#include <stdio.h>
+int main() {
+    int n, i;
+    long long fact = 1;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    
+    for (i = 1; i <= n; i++)
+        fact *= i;
+    
+    printf("Factorial = %lld", fact);
+    return 0;
+}
