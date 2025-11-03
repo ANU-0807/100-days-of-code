af19f555c888c17c3ab3ffb6662870866c7644c6
ANU-0807 <anoushkasaini2007@gmail.com>
2025-11-03 10:22:28 +0530

    Update B.c

--- DAY 19/B.c
+++ DAY 19/B.c
@@ -1,1 +1,15 @@
+// Q38. Write a program to find the sum of the digits of a number.
 
+#include <stdio.h>
+int main() {
+    int n, sum = 0, digit;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    while (n != 0) {
+        digit = n % 10;
+        sum += digit;
+        n /= 10;
+    }
+    printf("Sum of digits = %d", sum);
+    return 0;
+}
