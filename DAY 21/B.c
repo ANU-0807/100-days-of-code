7ee330ecd1b036697557ecf810917000bab93f78
ANU-0807 <anoushkasaini2007@gmail.com>
2025-11-27 10:12:10 +0530

    Update B.c

--- DAY 21/B.c
+++ DAY 21/B.c
@@ -1,1 +1,17 @@
+//Q42. Write a program to check if a number is a perfect number.//
 
+#include <stdio.h>
+int main() {
+    int num, i, sum = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    for (i = 1; i < num; i++) {    
+        if (num % i == 0)
+            sum += i;
+    }
+    if (sum == num)
+        printf("%d is a Perfect number\n", num);
+    else
+        printf("%d is not a Perfect number\n", num);
+    return 0;
+}
