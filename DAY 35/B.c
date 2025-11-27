df468ce33e4e04bfa6280445fccb03910d7fcfc9
ANU-0807 <anoushkasaini2007@gmail.com>
2025-11-27 11:15:41 +0530

    Update B.c

--- DAY 35/B.c
+++ DAY 35/B.c
@@ -1,1 +1,19 @@
+//Q70. Rotate an array to the right by k positions.//
+
+#include <stdio.h>
+int main() {
+    int n, i, k, temp;
+    scanf("%d", &n);
+    int a[n];
+    for(i = 0; i < n; i++) scanf("%d", &a[i]);
+    scanf("%d", &k);
+    k = k % n;
+    for(i = 0; i < k; i++) {
+        temp = a[n-1];
+        for(int j = n-1; j > 0; j--) a[j] = a[j-1];
+        a[0] = temp;
+    }
+    for(i = 0; i < n; i++) printf("%d ", a[i]);
+    return 0;
+}
 
