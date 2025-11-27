a185700543ff5cc4a9ade54777d09668f42aa693
ANU-0807 <anoushkasaini2007@gmail.com>
2025-11-27 10:42:13 +0530

    Update B.c

--- DAY 26/B.c
+++ DAY 26/B.c
@@ -1,1 +1,33 @@
+//Q52. Write a program to print the following pattern:
 
+*
+
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+*//
+
+#include <stdio.h>
+int main() {
+    int i, j;
+    int rows[] = {1, 3, 5, 3, 1};
+    for (i = 0; i < 5; i++) {
+        for (j = 0; j < rows[i]; j++) {
+            printf("*\n");
+        }
+        if (i != 4)      
+            printf("\n");
+    }
+    return 0;
+}
