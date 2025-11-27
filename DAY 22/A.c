dfaca9119fe5edfb42699abbf0d8f18969e2b3e9
ANU-0807 <anoushkasaini2007@gmail.com>
2025-11-27 10:16:19 +0530

    Update A.c

--- DAY 22/A.c
+++ DAY 22/A.c
@@ -1,1 +1,23 @@
+//Q43. Write a program to check if a number is a strong number.//
 
+#include <stdio.h>
+int main() {
+    int num, temp, digit, fact, i, sum = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    temp = num; 
+    while (temp > 0) {
+        digit = temp % 10;   
+        fact = 1;
+        for (i = 1; i <= digit; i++) {  
+            fact *= i;
+        }
+        sum += fact;  
+        temp /= 10;  
+    }
+    if (sum == num)
+        printf("%d is a Strong number\n", num);
+    else
+        printf("%d is not a Strong number\n", num);
+return 0;
+}
