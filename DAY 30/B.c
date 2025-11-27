40665bd83c5280f6d823d60e0f710d56f8ec4bc9
ANU-0807 <anoushkasaini2007@gmail.com>
2025-11-27 10:55:53 +0530

    Update B.c

--- DAY 30/B.c
+++ DAY 30/B.c
@@ -1,1 +1,17 @@
+//Q60. Count positive, negative, and zero elements in an array.//
+
+#include <stdio.h>
+int main() {
+    int n, i, pos = 0, neg = 0, zero = 0;
+    scanf("%d", &n);
+    int a[n];
+    for(i = 0; i < n; i++) scanf("%d", &a[i]);
+    for(i = 0; i < n; i++) {
+        if(a[i] > 0) pos++;
+        else if(a[i] < 0) neg++;
+        else zero++;
+    }
+    printf("Positive=%d Negative=%d Zero=%d", pos, neg, zero);
+    return 0;
+}
 
