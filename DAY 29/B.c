891c330c1df89f69549f3203cad4270d49858011
ANU-0807 <anoushkasaini2007@gmail.com>
2025-11-27 10:54:27 +0530

    Update B.c

--- DAY 29/B.c
+++ DAY 29/B.c
@@ -1,1 +1,16 @@
+//Q58. Find the maximum and minimum elements in an array.//
 
+#include <stdio.h>
+int main() {
+    int n, i;
+    scanf("%d", &n);
+    int a[n];
+    for(i = 0; i < n; i++) scanf("%d", &a[i]);
+    int max = a[0], min = a[0];
+    for(i = 1; i < n; i++) {
+        if(a[i] > max) max = a[i];
+        if(a[i] < min) min = a[i];
+    }
+    printf("Max=%d Min=%d", max, min);
+    return 0;
+}
