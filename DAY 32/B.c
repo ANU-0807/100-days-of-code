54b37ca8cc1ed11065e6798d41d6e06e3bf9a747
ANU-0807 <anoushkasaini2007@gmail.com>
2025-11-27 11:11:30 +0530

    Update B.c

--- DAY 32/B.c
+++ DAY 32/B.c
@@ -1,1 +1,22 @@
+//Q64. Find the digit that occurs the most times in an integer number.//
+
+#include <stdio.h>
+int main() {
+    int n, i, max = 0, digit;
+    scanf("%d", &n);
+    int count[10] = {0};
+    if(n < 0) n = -n;
+    while(n > 0) {
+        count[n%10]++;
+        n /= 10;
+    }
+    for(i = 0; i < 10; i++) {
+        if(count[i] > max) {
+            max = count[i];
+            digit = i;
+        }
+    }
+    printf("%d", digit);
+    return 0;
+}
 
