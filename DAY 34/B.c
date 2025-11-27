93104e83edbfbe9c3905ed5f232d0151b530316f
ANU-0807 <anoushkasaini2007@gmail.com>
2025-11-27 11:14:21 +0530

    Update B.c

--- DAY 34/B.c
+++ DAY 34/B.c
@@ -1,1 +1,14 @@
+//Q68. Delete an element from an array.//
+
+#include <stdio.h>
+int main() {
+    int n, i, pos;
+    scanf("%d", &n);
+    int a[n];
+    for(i = 0; i < n; i++) scanf("%d", &a[i]);
+    scanf("%d", &pos);
+    for(i = pos-1; i < n-1; i++) a[i] = a[i+1];
+    for(i = 0; i < n-1; i++) printf("%d ", a[i]);
+    return 0;
+}
 
