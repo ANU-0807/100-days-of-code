fc339c40f2cf74b801f83ef553b286a007f7c22a
ANU-0807 <anoushkasaini2007@gmail.com>
2025-11-27 10:55:05 +0530

    Update A.c

--- DAY 30/A.c
+++ DAY 30/A.c
@@ -1,1 +1,16 @@
+//Q59. Count even and odd numbers in an array.//
+
+#include <stdio.h>
+int main() {
+    int n, i, even = 0, odd = 0;
+    scanf("%d", &n);
+    int a[n];
+    for(i = 0; i < n; i++) scanf("%d", &a[i]);
+    for(i = 0; i < n; i++) {
+        if(a[i] % 2 == 0) even++;
+        else odd++;
+    }
+    printf("Even=%d Odd=%d", even, odd);
+    return 0;
+}
 
