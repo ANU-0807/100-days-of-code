33b71f7bc92bad22f328c762d796e10f10729ba0
ANU-0807 <anoushkasaini2007@gmail.com>
2025-11-27 10:52:48 +0530

    Update B.c

--- DAY 28/B.c
+++ DAY 28/B.c
@@ -1,1 +1,13 @@
+//Q56. Read and print elements of a one-dimensional array.//
+
+#include <stdio.h>
+int main() {
+    int n, i;
+    scanf("%d", &n);
+    int a[n];
+    for(i = 0; i < n; i++) scanf("%d", &a[i]);
+    for(i = 0; i < n; i++) printf("%d ", a[i]);
+    return 0;
+}
+
 
