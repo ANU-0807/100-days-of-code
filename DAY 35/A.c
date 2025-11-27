c4079ccca4fa7a0539ad02794dc6e8167b356b61
ANU-0807 <anoushkasaini2007@gmail.com>
2025-11-27 11:15:06 +0530

    Update A.c

--- DAY 35/A.c
+++ DAY 35/A.c
@@ -1,1 +1,19 @@
+//Q69. Find the second largest element in an array.//
+
+#include <stdio.h>
+int main() {
+    int n, i, max1, max2;
+    scanf("%d", &n);
+    int a[n];
+    for(i = 0; i < n; i++) scanf("%d", &a[i]);
+    max1 = max2 = -2147483648;
+    for(i = 0; i < n; i++) {
+        if(a[i] > max1) {
+            max2 = max1;
+            max1 = a[i];
+        } else if(a[i] > max2 && a[i] != max1) max2 = a[i];
+    }
+    printf("%d", max2);
+    return 0;
+}
 
