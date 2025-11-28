ff6ab2011dab5541b9953087a066f6404b69c81b
ANU-0807 <anoushkasaini2007@gmail.com>
2025-11-28 10:30:59 +0530

    Update B.c

--- DAY 44/B.c
+++ DAY 44/B.c
@@ -1,1 +1,16 @@
+//Q88. Replace spaces with hyphens in a string.//
+
+#include <stdio.h>
+int main() {
+    char s[200];
+    int i = 0;
+    gets(s);
+    while (s[i] != '\0') {
+        if (s[i] == ' ')
+            s[i] = '-';
+        i++;
+    }
+    puts(s);
+    return 0;
+}
 
