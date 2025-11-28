7cd091c62312290d243261e35e6eae8cc1ba17bc
ANU-0807 <anoushkasaini2007@gmail.com>
2025-11-28 10:40:10 +0530

    Update B.c

--- DAY 46/B.c
+++ DAY 46/B.c
@@ -1,1 +1,22 @@
+//Q92. Find the first repeating lowercase alphabet in a string.//
+
+#include <stdio.h>
+int main() {
+    char s[200];
+    int freq[26] = {0}, i = 0;
+    gets(s);
+    while (s[i] != '\0') {
+        char ch = s[i];
+        if (ch >= 'a' && ch <= 'z') {
+            if (freq[ch - 'a'] == 1) {
+                printf("%c\n", ch);
+                return 0;
+            }
+            freq[ch - 'a']++;
+        }
+        i++;
+    }
+    printf("No repeating lowercase alphabet\n");
+    return 0;
+}
 
