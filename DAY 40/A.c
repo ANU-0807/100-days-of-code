9baef85062a7a4517dfec9054c7ea07e2c74a01d
ANU-0807 <anoushkasaini2007@gmail.com>
2025-11-28 10:22:23 +0530

    Update A.c

--- DAY 40/A.c
+++ DAY 40/A.c
@@ -1,1 +1,16 @@
+//Q79. Perform a diagonal traversal of a matrix.//
+
+#include <stdio.h>
+int main() {
+    int a[10][10], r, c, i, j;
+    scanf("%d %d", &r, &c);
+    for (i = 0; i < r; i++)
+        for (j = 0; j < c; j++)
+            scanf("%d", &a[i][j]);
+    for (i = 0; i < r; i++) {
+        printf("%d ", a[i][i]);
+    }
+    return 0;
+}
+
 
