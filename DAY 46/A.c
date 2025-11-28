b1fe56bd4b5b083ade8f815a536f43fbfb03cbca
ANU-0807 <anoushkasaini2007@gmail.com>
2025-11-28 10:39:30 +0530

    Update A.c

--- DAY 46/A.c
+++ DAY 46/A.c
@@ -1,1 +1,17 @@
+//Q91.Remove all vowels from a string.//
+
+#include <stdio.h>
+int main() {
+    char s[200], out[200];
+    int i = 0, j = 0;
+    gets(s);
+    while (s[i] != '\0') {
+        char ch = s[i] | 32;
+        if (ch!='a' && ch!='e' && ch!='i' && ch!='o' && ch!='u')
+            out[j++] = s[i];
+        i++;
+    }
+    ou
+
+
 
