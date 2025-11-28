763d33870d0c8d0f55a44b957c32bb5f15933fda
ANU-0807 <anoushkasaini2007@gmail.com>
2025-11-28 10:30:21 +0530

    Update A.c

--- DAY 44/A.c
+++ DAY 44/A.c
@@ -1,1 +1,20 @@
+//Q87. Count spaces, digits, and special characters in a string.//
+
+#include <stdio.h>
+int main() {
+    char s[200];
+    int i = 0, sp = 0, d = 0, sc = 0;
+    gets(s);
+    while (s[i] != '\0') {
+        if (s[i] == ' ')
+            sp++;
+        else if (s[i] >= '0' && s[i] <= '9')
+            d++;
+        else if (!((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z')))
+            sc++;
+        i++;
+    }
+    printf("Spaces = %d\nDigits = %d\nSpecial chars = %d\n", sp, d, sc);
+    return 0;
+}
 
