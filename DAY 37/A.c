171cf1265c673490b0ee764e04a6e951faa45edb
ANU-0807 <anoushkasaini2007@gmail.com>
2025-11-28 10:16:13 +0530

    Update A.c

--- DAY 37/A.c
+++ DAY 37/A.c
@@ -1,1 +1,20 @@
+//Q73. Find the sum of each row of a matrix and store it in an array.//
+
+#include <stdio.h>
+int main() {
+    int a[10][10], r, c, i, j;
+    int rowSum[10];
+    scanf("%d %d", &r, &c);
+    for (i = 0; i < r; i++)
+        for (j = 0; j < c; j++)
+            scanf("%d", &a[i][j]);
+    for (i = 0; i < r; i++) {
+        rowSum[i] = 0;
+        for (j = 0; j < c; j++)
+            rowSum[i] += a[i][j];
+    }
+    for (i = 0; i < r; i++)
+        printf("Row %d sum = %d\n", i + 1, rowSum[i]);
+    return 0;
+}
 
