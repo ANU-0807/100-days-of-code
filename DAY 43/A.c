af37767ef167d9603a2ea2c546804d158f0aaa22
ANU-0807 <anoushkasaini2007@gmail.com>
2025-11-28 10:27:52 +0530

    Update A.c

--- DAY 43/A.c
+++ DAY 43/A.c
@@ -1,1 +1,19 @@
+//Q85. Reverse a string.//
+
+#include <stdio.h>
+int main() {
+    char s[100];
+    int i = 0, j, len = 0;
+    gets(s);
+    while (s[len] != '\0')
+        len++;
+    j = len - 1;
+    for (i = 0; i < j; i++, j--) {
+        char temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+    }
+    puts(s);
+    return 0;
+}
 
