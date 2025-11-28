f4d225f456dfdcbd9f27e39a05302452bd8d94ea
ANU-0807 <anoushkasaini2007@gmail.com>
2025-11-28 10:29:25 +0530

    Update B.c

--- DAY 43/B.c
+++ DAY 43/B.c
@@ -1,1 +1,18 @@
+//Q86. Check if a string is a palindrome.//
 
+#include <stdio.h>
+int main() {
+    char s[100];
+    int i = 0, j, len = 0, flag = 1;
+    gets(s);
+    while (s[len] != '\0')
+        len++;
+    j = len - 1;
+    for (i = 0; i < j; i++, j--) {
+        if (s[i] != s[j])
+            flag = 0;
+    }
+    if (flag) printf("Palindrome\n");
+    else printf("Not palindrome\n");
+    return 0;
+}
