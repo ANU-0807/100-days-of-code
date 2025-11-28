ea55257b31d20902bc6204d8069efed19a0dfe76
ANU-0807 <anoushkasaini2007@gmail.com>
2025-11-28 10:25:18 +0530

    Update B.c

--- DAY 41/B.c
+++ DAY 41/B.c
@@ -1,1 +1,14 @@
+//Q82. Print each character of a string on a new line.//
+
+#include <stdio.h>
+int main() {
+    char s[100];
+    int i = 0;
+    gets(s);
+    while (s[i] != '\0') {
+        printf("%c\n", s[i]);
+        i++;
+    }
+    return 0;
+}
 
