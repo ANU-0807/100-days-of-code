027a1702430545e6c6929ee9b66fe7fe3edc14d7
ANU-0807 <anoushkasaini2007@gmail.com>
2025-11-28 10:32:05 +0530

    Update A.c

--- DAY 45/A.c
+++ DAY 45/A.c
@@ -1,1 +1,17 @@
+//Q89. Count the frequency of a given character in a string.//
+
+#include <stdio.h>
+int main() {
+    char s[200], ch;
+    int i = 0, count = 0;
+    gets(s);
+    scanf("%c", &ch);
+    while (s[i] != '\0') {
+        if (s[i] == ch)
+            count++;
+        i++;
+    }
+    printf("Frequency = %d\n", count);
+    return 0;
+}
 
