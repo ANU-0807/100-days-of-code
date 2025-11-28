8b8e8590b816936f0334e72aa86cbe485e87c78f
ANU-0807 <anoushkasaini2007@gmail.com>
2025-11-28 10:24:35 +0530

    Update A.c

--- DAY 41/A.c
+++ DAY 41/A.c
@@ -1,1 +1,13 @@
+//Q81. Count characters in a string without using built-in length functions.//
+
+#include <stdio.h>
+int main() {
+    char s[100];
+    int i = 0;
+    gets(s);
+    while (s[i] != '\0')
+        i++;
+    printf("Length = %d\n", i);
+    return 0;
+}
 
