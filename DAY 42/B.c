a83e70ff443cea11bdf1e3f64f0c41a586087537
ANU-0807 <anoushkasaini2007@gmail.com>
2025-11-28 10:27:04 +0530

    Update B.c

--- DAY 42/B.c
+++ DAY 42/B.c
@@ -1,1 +1,16 @@
+//Q84. Convert a lowercase string to uppercase without using built-in functions.//
+
+#include <stdio.h>
+int main() {
+    char s[100];
+    int i = 0;
+    gets(s);
+    while (s[i] != '\0') {
+        if (s[i] >= 'a' && s[i] <= 'z')
+            s[i] = s[i] - 32;
+        i++;
+    }
+    puts(s);
+    return 0;
+}
 
