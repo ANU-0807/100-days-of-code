141e7b0c0e69ed26e2dac0a6fd3a3965d2edd584
Anoushka_Saini <anoushkasaini2007@gmail.com>
2025-12-02 23:53:29 +0530

    Update B.c

--- DAY 49/B.c
+++ DAY 49/B.c
@@ -1,1 +1,19 @@
+//Q98. Print the initials of a name with the surname displayed in full.//
+
+#include <stdio.h>
+int main() {
+    char s[200];
+    int i = 0, last = 0;
+    gets(s);
+    printf("%c ", s[0]);
+    while (s[i] != '\0') {
+        if (s[i] == ' ' && s[i+1] != ' ') {
+            last = i + 1;
+            printf("%c ", s[last]);
+        }
+        i++;
+    }
+    printf("%s", &s[last]);
+    return 0;
+}
 
