47ecc7eeac9bec49ca832effcf40d1d052244d14
Anoushka_Saini <anoushkasaini2007@gmail.com>
2025-12-02 23:48:34 +0530

    Update B.c

--- DAY 47/B.c
+++ DAY 47/B.c
@@ -1,1 +1,29 @@
+//Q94. Find the longest word in a sentence.
+
+#include <stdio.h>
+int main() {
+    char s[300];
+    char longest[100];
+    int i = 0, len = 0, maxLen = 0, start = 0;
+    gets(s);
+    while (1) {
+        if (s[i] == ' ' || s[i] == '\0') {
+            if (len > maxLen) {
+                maxLen = len;
+                int k, idx = start;
+                for (k = 0; k < len; k++)
+                    longest[k] = s[idx + k];
+                longest[len] = '\0';
+            }
+            len = 0;
+            start = i + 1;
+            if (s[i] == '\0') break;
+        } else {
+            len++;
+        }
+        i++;
+    }
+    puts(longest);
+    return 0;
+}
 
