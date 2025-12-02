0772325c71739138008dfd8f2d8eb757539ef368
Anoushka_Saini <anoushkasaini2007@gmail.com>
2025-12-02 23:55:53 +0530

    Update B.c

--- DAY 50/B.c
+++ DAY 50/B.c
@@ -1,1 +1,16 @@
+///Q100. Print all substrings of a string.//
 
+#include <stdio.h>
+int main() {
+    char s[200];
+    int i, j, k;
+    gets(s);
+    for (i = 0; s[i] != '\0'; i++) {
+        for (j = i; s[j] != '\0'; j++) {
+            for (k = i; k <= j; k++)
+                printf("%c", s[k]);
+            printf("\n");
+        }
+    }
+    return 0;
+}
