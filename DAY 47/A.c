2b19ea7be700e9e2882f6b7b50fd37a8ddbc6b9e
Anoushka_Saini <anoushkasaini2007@gmail.com>
2025-12-02 23:47:57 +0530

    Update A.c

--- DAY 47/A.c
+++ DAY 47/A.c
@@ -1,1 +1,21 @@
+//Q93. Check if two strings are anagrams of each other.
+
+#include <stdio.h>
+int main() {
+    char a[100], b[100];
+    int c1[256] = {0}, c2[256] = {0}, i = 0;
+    gets(a);
+    gets(b);
+    while (a[i] != '\0') { c1[a[i]]++; i++; }
+    i = 0;
+    while (b[i] != '\0') { c2[b[i]]++; i++; }
+    for (i = 0; i < 256; i++) {
+        if (c1[i] != c2[i]) {
+            printf("Not anagrams\n");
+            return 0;
+        }
+    }
+    printf("Anagrams\n");
+    return 0;
+}
 
