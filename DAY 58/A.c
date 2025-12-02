c0efd2d5d63e8c70b3350f3e5ee60c30ec2645e6
Anoushka_Saini <anoushkasaini2007@gmail.com>
2025-12-03 00:14:37 +0530

    Update A.c

--- DAY 58/A.c
+++ DAY 58/A.c
@@ -1,1 +1,19 @@
+//Q108. Write a Program to take an integer array nums. Print an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i]. The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.//
+
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+    int a[n], ans[n];
+    for (int i = 0; i < n; i++) scanf("%d", &a[i]);
+    for (int i = 0; i < n; i++) {
+        ans[i] = 1;
+        for (int j = 0; j < n; j++)
+            if (i != j)
+                ans[i] *= a[j];
+    }
+    for (int i = 0; i < n; i++)
+        printf("%d ", ans[i]);
+    return 0;
+}
 
