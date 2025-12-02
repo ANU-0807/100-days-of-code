56941d200d2667b6ae9e9ff0ad2c37900dfe7857
Anoushka_Saini <anoushkasaini2007@gmail.com>
2025-12-03 00:16:37 +0530

    Update A.c

--- DAY 60/A.c
+++ DAY 60/A.c
@@ -1,1 +1,17 @@
+//Q110. Write a program to take an integer array arr and an integer k as inputs. The task is to find the maximum element in each subarray of size k moving from left to right. Print the maximum elements for each window separated by spaces as output.//
+
+#include <stdio.h>
+int main() {
+    int n, k;
+    scanf("%d %d", &n, &k);
+    int a[n];
+    for (int i = 0; i < n; i++) scanf("%d", &a[i]);
+    for (int i = 0; i <= n - k; i++) {
+        int max = a[i];
+        for (int j = i; j < i + k; j++)
+            if (a[j] > max) max = a[j];
+        printf("%d ", max);
+    }
+    return 0;
+}
 
