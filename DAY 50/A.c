ee16dbdcb857eb71f425d8358a8438b21c03b3b7
Anoushka_Saini <anoushkasaini2007@gmail.com>
2025-12-02 23:54:47 +0530

    Update A.c

--- DAY 50/A.c
+++ DAY 50/A.c
@@ -1,1 +1,14 @@
+//Q99. Change the date format from dd/04/yyyy to dd-Apr-yyyy.//
+
+#include <stdio.h>
+int main() {
+    char date[20];
+    char month[12][4] = {"Jan", "Feb", "Mar", "Apr"," May", "Jun",
+                         "Jul","Aug", "Sep","Oct", "Nov" ,"Dec"};
+    int d, m, y;
+    gets(date);
+    sscanf(date, "%d/%d/%d", &d, &m, &y);
+    printf("%02d-%s-%d\n", d, month[m-1], y);
+    return 0;
+}
 
