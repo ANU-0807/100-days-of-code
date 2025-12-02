beabce997af1c5f11d8199ae0d4c95992f9446f6
Anoushka_Saini <anoushkasaini2007@gmail.com>
2025-12-02 23:49:30 +0530

    Update A.c

--- DAY 48/A.c
+++ DAY 48/A.c
@@ -1,1 +1,21 @@
+//Q95. Check if one string is a rotation of another.
+
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char a[100], b[100], temp[200];
+    gets(a);
+    gets(b);
+    if (strlen(a) != strlen(b)) {
+        printf("Not rotation\n");
+        return 0;
+    }
+    strcpy(temp, a);
+    strcat(temp, a);
+    if (strstr(temp, b))
+        printf("Rotation\n");
+    else
+        printf("Not rotation\n");
+    return 0;
+}
 
