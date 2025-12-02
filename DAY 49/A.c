7437e7a680cee93e6afabd0f9607907683b7c9d7
Anoushka_Saini <anoushkasaini2007@gmail.com>
2025-12-02 23:51:07 +0530

    Update A.c

--- DAY 49/A.c
+++ DAY 49/A.c
@@ -1,1 +1,17 @@
+//Q97. Print the initials of a name.//
+
+#include <stdio.h>
+int main() {
+    char s[200];
+    int i = 0;
+    gets(s);
+    if (s[0] != ' ')
+        printf("%c ", s[0]);
+    while (s[i] != '\0') {
+        if (s[i] == ' ' && s[i+1] != '\0' && s[i+1] != ' ')
+            printf("%c ", s[i+1]);
+        i++;
+    }
+    return 0;
+}
 
