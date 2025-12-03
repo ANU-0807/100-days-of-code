e7de76150b63ffe424c2b404ff105ec8182f0139
Anoushka_Saini <anoushkasaini2007@gmail.com>
2025-12-03 13:34:38 +0530

    Update A.c

--- DAY 95/A.c
+++ DAY 95/A.c
@@ -1,1 +1,17 @@
+//Q145. Return a structure containing top student's details from a function.//
+
+#include <stdio.h>
+struct Student {
+    char name[20];
+    float marks;
+};
+struct Student topStudent() {
+    struct Student s = {"Aman", 95.5};
+    return s;
+}
+int main() {
+    struct Student t = topStudent();
+    printf("%s %.2f", t.name, t.marks);
+    return 0;
+}
 
