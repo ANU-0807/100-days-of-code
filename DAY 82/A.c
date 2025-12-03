364083aca0653f22471a7f85b7a93e620cf60938
Anoushka_Saini <anoushkasaini2007@gmail.com>
2025-12-03 10:52:59 +0530

    Update A.c

--- DAY 82/A.c
+++ DAY 82/A.c
@@ -1,1 +1,12 @@
+//Q132. Define an enum for traffic lights (RED, YELLOW, GREEN) and print 'Stop', 'Wait', or 'Go' based on its value.//
+
+#include <stdio.h>
+enum Light {RED, YELLOW, GREEN};
+int main() {
+    enum Light l = GREEN;
+    if (l == RED) printf("Stop");
+    else if (l == YELLOW) printf("Wait");
+    else printf("Go");
+    return 0;
+}
 
