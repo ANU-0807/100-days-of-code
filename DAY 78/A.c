4003975ff80c6c630dc6911eebc80f15a8a8d33c
Anoushka_Saini <anoushkasaini2007@gmail.com>
2025-12-03 10:47:21 +0530

    Update A.c

--- DAY 78/A.c
+++ DAY 78/A.c
@@ -1,1 +1,27 @@
+//Q128. Read a text file and count how many vowels and consonants are in the file. Ignore digits and special characters.//
+
+#include <stdio.h>
+int main() {
+    FILE *fp;
+    char ch;
+    int vowels = 0, consonants = 0;
+    fp = fopen("input.txt", "r");
+    if (fp == NULL) {
+        printf("File not found.\n");
+        return 0;
+    }
+    while ((ch = fgetc(fp)) != EOF) {
+        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+            char t = ch | 32;
+            if (t=='a'||t=='e'||t=='i'||t=='o'||t=='u')
+                vowels++;
+            else
+                consonants++;
+        }
+    }
+    fclose(fp);
+    printf("Vowels: %d\n", vowels);
+    printf("Consonants: %d\n", consonants);
+    return 0;
+}
 
