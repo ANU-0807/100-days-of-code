a87be22acbab95121626ad6c675ce116f071b5f0
Anoushka_Saini <anoushkasaini2007@gmail.com>
2025-12-03 13:27:39 +0530

    Update A.c

--- DAY 89/A.c
+++ DAY 89/A.c
@@ -1,1 +1,9 @@
+//Q139. Show that enums store integers by printing assigned values.//
+
+#include <stdio.h>
+enum Test {X=5, Y=15};
+int main() {
+    printf("X = %d\nY = %d", X, Y);
+    return 0;
+}
 
