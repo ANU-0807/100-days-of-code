2492937df31ce3d6a8cbcecf731022621931bf82
Anoushka_Saini <anoushkasaini2007@gmail.com>
2025-12-03 10:58:40 +0530

    Update A.c

--- DAY 84/A.c
+++ DAY 84/A.c
@@ -1,1 +1,12 @@
+//Q134. Define an enum with SUCCESS, FAILURE, and TIMEOUT, and print messages accordingly.//
+
+#include <stdio.h>
+enum Status {SUCCESS, FAILURE, TIMEOUT};
+int main() {
+    enum Status s = SUCCESS;
+    if (s == SUCCESS) printf("Operation Successful");
+    else if (s == FAILURE) printf("Operation Failed");
+    else printf("Operation Timed Out");
+    return 0;
+}
 
