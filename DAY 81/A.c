f628033ac744cdda7aece9080491996d4109e4dc
Anoushka_Saini <anoushkasaini2007@gmail.com>
2025-12-03 10:51:49 +0530

    Update A.c

--- DAY 81/A.c
+++ DAY 81/A.c
@@ -1,1 +1,10 @@
+//Q131. Create an enumeration for days (SUNDAY to SATURDAY) and print each day with its integer value.//
+
+#include <stdio.h>
+enum Day {SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY};
+int main() {
+    for (int i = SUNDAY; i <= SATURDAY; i++)
+        printf("%d\n", i);
+    return 0;
+}
 
