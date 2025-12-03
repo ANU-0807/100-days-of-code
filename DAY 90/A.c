f0097a7e2d5dbae1f03b41ca170505c4baf67bb9
Anoushka_Saini <anoushkasaini2007@gmail.com>
2025-12-03 13:28:58 +0530

    Update A.c

--- DAY 90/A.c
+++ DAY 90/A.c
@@ -1,1 +1,15 @@
+//Q140. Define a struct with enum Gender and print person's gender.//
+
+#include <stdio.h>
+enum Gender {MALE, FEMALE};
+struct Person {
+    char name[20];
+    enum Gender g;
+};
+int main() {
+    struct Person p = {"Rahul", MALE};
+    if (p.g == MALE) printf("Gender: Male");
+    else printf("Gender: Female");
+    return 0;
+}
 
