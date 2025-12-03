6d222b93c177ae4ce8250c72704638139c655dd9
Anoushka_Saini <anoushkasaini2007@gmail.com>
2025-12-03 13:27:05 +0530

    Update A.c

--- DAY 88/A.c
+++ DAY 88/A.c
@@ -1,1 +1,10 @@
+//Q138. Print all enum names and integer values using a loop.//
+
+#include <stdio.h>
+enum Color {RED, BLUE, GREEN};
+int main() {
+    for (int i = RED; i <= GREEN; i++)
+        printf("%d\n", i);
+    return 0;
+}
 
