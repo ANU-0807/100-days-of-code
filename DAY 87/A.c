14231353ba5c3a721fe2a6ef90fcdb8d3639a8c3
Anoushka_Saini <anoushkasaini2007@gmail.com>
2025-12-03 13:24:19 +0530

    Update A.c

--- DAY 87/A.c
+++ DAY 87/A.c
@@ -1,1 +1,12 @@
+//Q137. Create an enum for user roles (ADMIN, USER, GUEST) and display messages based on role.//
+
+#include <stdio.h>
+enum Role {ADMIN, USER, GUEST};
+int main() {
+    enum Role r = ADMIN;
+    if (r == ADMIN) printf("Admin Access");
+    else if (r == USER) printf("User Access");
+    else printf("Guest Access");
+    return 0;
+}
 
