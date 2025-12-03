d4306dcd0bd59dc3e4dbbe9a6f0ec9741a77f88f
Anoushka_Saini <anoushkasaini2007@gmail.com>
2025-12-03 10:57:43 +0530

    Update A.c

--- DAY 83/A.c
+++ DAY 83/A.c
@@ -1,1 +1,11 @@
+//Q133. Create an enum for months and print how many days each month has.//
+
+#include <stdio.h>
+enum Month {JAN=1, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC};
+int main() {
+    printf("February has 28 days\n");
+    printf("April has 30 days\n");
+    printf("January has 31 days\n");
+    return 0;
+}
 
