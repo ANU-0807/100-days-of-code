8ed50b689adb7f596d3a8481ef3214fbb0ef9561
Anoushka_Saini <anoushkasaini2007@gmail.com>
2025-12-03 10:59:25 +0530

    Update A.c

--- DAY 85/A.c
+++ DAY 85/A.c
@@ -1,1 +1,9 @@
+//Q135. Assign explicit values starting from 10 and print them.//
+
+#include <stdio.h>
+enum Num {A=10, B, C, D};
+int main() {
+    printf("%d %d %d %d", A, B, C, D);
+    return 0;
+}
 
