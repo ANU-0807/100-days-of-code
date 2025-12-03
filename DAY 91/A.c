55c6e097b6377ab7b1024256f130fffdc4917d13
Anoushka_Saini <anoushkasaini2007@gmail.com>
2025-12-03 13:29:51 +0530

    Update A.c

--- DAY 91/A.c
+++ DAY 91/A.c
@@ -1,1 +1,15 @@
+//Q141. Define a structure Student with name, roll_no, and marks, then read and print one student's data.//
+
+#include <stdio.h>
+struct Student {
+    char name[20];
+    int roll;
+    float marks;
+};
+int main() {
+    struct Student s;
+    scanf("%s %d %f", s.name, &s.roll, &s.marks);
+    printf("%s %d %.2f", s.name, s.roll, s.marks);
+    return 0;
+}
 
