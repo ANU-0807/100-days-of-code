0124855a387dd312c3d3ceb96ea840effeca50e6
Anoushka_Saini <anoushkasaini2007@gmail.com>
2025-12-03 13:36:11 +0530

    Update A.c

--- DAY 96/A.c
+++ DAY 96/A.c
@@ -1,1 +1,16 @@
-DAY
+//Q146. Create Employee structure with nested Date structure for joining date and print details.//
+
+#include <stdio.h>
+struct Date {
+    int d, m, y;
+};
+struct Employee {
+    int id;
+    struct Date join;
+};
+int main() {
+    struct Employee e = {1, {10, 5, 2024}};
+    printf("ID: %d\nDate: %d/%d/%d", e.id, e.join.d, e.join.m, e.join.y);
+    return 0;
+}
+
