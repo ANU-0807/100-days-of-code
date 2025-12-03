fc4aeb9dcbe94ce9a8e667e8c17a5c996229db2a
Anoushka_Saini <anoushkasaini2007@gmail.com>
2025-12-03 13:39:47 +0530

    Update A.c

--- DAY 98/A.c
+++ DAY 98/A.c
@@ -1,1 +1,17 @@
+//Q 148. Take two structs as input and check if they are identical.//
+
+#include <stdio.h>
+struct Data {
+    int x;
+    float y;
+};
+int main() {
+    struct Data a = {2, 3.5}, b = {2, 3.5};
+    if (a.x == b.x && a.y == b.y)
+        printf("Identical");
+    else
+        printf("Not Identical");
+    return 0;
+}
+
 
