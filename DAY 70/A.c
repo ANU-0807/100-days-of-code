a8a6800bae531569a647e605adea2a2f78c8d652
Anoushka_Saini <anoushkasaini2007@gmail.com>
2025-12-03 10:24:22 +0530

    Update A.c

--- DAY 70/A.c
+++ DAY 70/A.c
@@ -1,1 +1,17 @@
+//Q120. Write a program to take a string input. Change it to sentence case.//
+
+#include <stdio.h>
+#include <ctype.h>
+int main() {
+    char s[200];
+    fgets(s, sizeof(s), stdin);
+    if (s[0] >= 'a' && s[0] <= 'z')
+        s[0] = s[0] - 32;
+    for (int i = 1; s[i] != '\0'; i++) {
+        if (s[i] >= 'A' && s[i] <= 'Z')
+            s[i] = s[i] + 32;
+    }
+    printf("Sentence Case: %s", s);
+    return 0;
+}
 
