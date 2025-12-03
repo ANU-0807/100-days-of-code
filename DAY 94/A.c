dbc4bd3ee5c7a6a531e7cc513dd94e7960d8557f
Anoushka_Saini <anoushkasaini2007@gmail.com>
2025-12-03 13:33:30 +0530

    Update A.c

--- DAY 94/A.c
+++ DAY 94/A.c
@@ -1,1 +1,16 @@
+//Q144. Write a function that accepts a structure as parameter and prints its members.//
+
+#include <stdio.h>
+struct Student {
+    char name[20];
+    int roll;
+};
+void display(struct Student s) {
+    printf("%s %d", s.name, s.roll);
+}
+int main() {
+    struct Student s = {"Riya", 10};
+    display(s);
+    return 0;
+}
 
