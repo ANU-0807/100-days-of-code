07d287a3a340451dc1dd4ed0950697c97b8cbbe3
Anoushka_Saini <anoushkasaini2007@gmail.com>
2025-12-03 13:37:28 +0530

    Update A.c

--- DAY 97/A.c
+++ DAY 97/A.c
@@ -1,1 +1,19 @@
+//Q147. Store employee data in a binary file using fwrite() and read using fread().//
+
+#include <stdio.h>
+struct Employee {
+    int id;
+};
+int main() {
+    FILE *fp;
+    struct Employee e = {101}, r;
+    fp = fopen("emp.dat", "wb");
+    fwrite(&e, sizeof(e), 1, fp);
+    fclose(fp);
+    fp = fopen("emp.dat", "rb");
+    fread(&r, sizeof(r), 1, fp);
+    printf("%d", r.id);
+    fclose(fp);
+    return 0;
+}
 
