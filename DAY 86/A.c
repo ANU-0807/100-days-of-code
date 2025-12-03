9d3fb84bea6f5419b57a4ab3e6a202bdc699496d
Anoushka_Saini <anoushkasaini2007@gmail.com>
2025-12-03 13:22:46 +0530

    Update A.c

--- DAY 86/A.c
+++ DAY 86/A.c
@@ -1,1 +1,15 @@
+//Q136. Use enum to represent menu choices (ADD, SUBTRACT, MULTIPLY) and perform operations using switch.//
+
+#include <stdio.h>
+enum Menu {ADD=1, SUBTRACT, MULTIPLY};
+int main() {
+    enum Menu m = MULTIPLY;
+    int a = 10, b = 5;
+    switch(m) {
+        case ADD: printf("%d", a+b); break;
+        case SUBTRACT: printf("%d", a-b); break;
+        case MULTIPLY: printf("%d", a*b); break;
+    }
+    return 0;
+}
 
