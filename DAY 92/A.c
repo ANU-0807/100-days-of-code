7ffae85577527426762061955d47d2bd5558db1d
Anoushka_Saini <anoushkasaini2007@gmail.com>
2025-12-03 13:31:32 +0530

    Update A.c

--- DAY 92/A.c
+++ DAY 92/A.c
@@ -1,1 +1,17 @@
+//Q142. Store details of 5 students in an array of structures and print all.//
+
+#include <stdio.h>
+struct Student {
+    char name[20];
+    int roll;
+    float marks;
+};
+int main() {
+    struct Student s[5];
+    for (int i = 0; i < 5; i++)
+        scanf("%s %d %f", s[i].name, &s[i].roll, &s[i].marks);
+    for (int i = 0; i < 5; i++)
+        printf("%s %d %.2f\n", s[i].name, s[i].roll, s[i].marks);
+    return 0;
+}
 
