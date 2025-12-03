4c2ade47eb4f710f08c7990f03500e0cac09a220
Anoushka_Saini <anoushkasaini2007@gmail.com>
2025-12-03 13:43:28 +0530

    Update A.c

--- DAY 100/A.c
+++ DAY 100/A.c
@@ -1,1 +1,14 @@
+// Q 150 Use pointer to struct to modify and display data using -> operator.//
+
+#include <stdio.h>
+struct Student {
+    int roll;
+};
+int main() {
+    struct Student s = {25};
+    struct Student *p = &s;
+    p->roll = 50;
+    printf("%d", p->roll);
+    return 0;
+}
 
